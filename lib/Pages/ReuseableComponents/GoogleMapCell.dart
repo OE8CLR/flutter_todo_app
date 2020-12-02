00b6f3f1d12cd005b388fcb461fede259ee86c5a
Christoph Lückler <c.lueckler@netconomy.net>
2020-12-02 14:14:51 +0100

    Fix lagging issue when opening TodoListDetails

--- lib/Pages/ReuseableComponents/GoogleMapCell.dart
+++ lib/Pages/ReuseableComponents/GoogleMapCell.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -16,35 +17,59 @@ class GoogleMapCell extends StatefulWidget {
 }
 
 class _GoogleMapCellState extends State<GoogleMapCell> {
+  final Completer<GoogleMapController> _mapController = Completer();
+  Future _mapFuture = Future.delayed(Duration(milliseconds: 250), () => true);
 
   @override
   Widget build(BuildContext context) {
+    // Initialisation of GoogleMaps produces some lagging in navigation animation
+    // There is a workaround available -> https://github.com/flutter/flutter/issues/28493
+    return FutureBuilder(
+        future: _mapFuture,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Container(
+              padding: widget.padding ?? EdgeInsets.zero,
+              height: widget.height,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+          return _buildMapWidget();
+        });
+  }
+
+  Widget _buildMapWidget() {
     var coordinates = LatLng(widget.latitude ?? 0, widget.longitude ?? 0);
     var marker = Marker(markerId: MarkerId("POI"), position: coordinates);
 
     return Container(
-        padding: widget.padding ?? EdgeInsets.zero,
-        height: widget.height,
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.grey,
-              width: 1.0,
-            ),
-          ),
-          child: GoogleMap(
-              mapType: MapType.normal,
-              myLocationButtonEnabled: false,
-              initialCameraPosition: CameraPosition(
-                target: coordinates,
-                zoom: widget.zoomLevel ?? 1.0,
-              ),
-              markers: [marker].toSet(),
-              rotateGesturesEnabled: !widget.readOnly,
-              zoomGesturesEnabled: !widget.readOnly,
-              scrollGesturesEnabled: !widget.readOnly,
+      padding: widget.padding ?? EdgeInsets.zero,
+      height: widget.height,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey,
+            width: 1.0,
           ),
         ),
+        child: GoogleMap(
+          mapType: MapType.normal,
+          myLocationButtonEnabled: false,
+          initialCameraPosition: CameraPosition(
+            target: coordinates,
+            zoom: widget.zoomLevel ?? 1.0,
+          ),
+          markers: [marker].toSet(),
+          rotateGesturesEnabled: !widget.readOnly,
+          zoomGesturesEnabled: !widget.readOnly,
+          scrollGesturesEnabled: !widget.readOnly,
+          onMapCreated: (GoogleMapController controller) {
+            _mapController.complete(controller);
+          },
+        ),
+      ),
     );
   }
 
